94766a2ec10bcd7109a49e77dcdad2db2e3a57b1
samirsarsia <sarsiasamir123@gmail.com>
2024-09-26 19:01:10 -0400

    new code

--- banana.py
+++ banana.py
@@ -1,1 +1,27 @@
-print("hello worl")
+'''
+def greeting(name):
+	print("Welcome to The Hundred Acre Wood! " + name + " My name is Christopher Robin.")
+	return 
+
+
+greeting("Michael")
+greeting("Winnie the Pooh")
+
+
+'''
+
+def sum_honey(hunny_jars):
+	total = 0
+	for num in range(0, len(hunny_jars)):
+		total += hunny_jars[num]
+
+	return total
+		
+		
+
+
+hunny_jars = [2, 3, 4, 5]
+print(sum_honey(hunny_jars))
+
+hunny_jars = []
+print(sum_honey(hunny_jars))
